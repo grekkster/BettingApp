a518792d2c189d3c1085e1ea6ef4743239de7d60
jura <jirapanek@gmail.com>
2018-06-19 15:32:31 +0200

    RouteConfig homepage changed to MatchesController

--- BettingApp/App_Start/RouteConfig.cs
+++ BettingApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BettingApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Matches", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
